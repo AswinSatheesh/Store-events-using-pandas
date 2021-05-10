d75947138b976b1182933a8448b9ccd6e7925d8d
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-05-10 18:34:31 +0530

    issue fixed

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import time
 import csv 
 from datetime import datetime
 import pandas as pd
+
 app = Flask(__name__)
+
 PORT=5000
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,19 +21,23 @@ def index():
 
 @app.route('/getdetails', methods=['GET', 'POST'])
 def getdetails():
-    global name
     if request.method == 'POST':
         name = request.form['name']
         date = request.form['date']
         date=str(date)
         
-        
-    df = pd.read_csv('event.csv')
-    df.append({'Event Name': name, 'Date': date },ignore_index=True)
+    
+    result = { 
+        'Event Name': name,
+        'Date': date 
+    }
+
+    df = pd.DataFrame([result.values()], columns=['Event Name', 'Date'])
+    df.to_csv('event.csv', mode='a', header=False, index=False)
         
               
               
-    return render_template('new.html', n=name, s=date) 
+    return redirect(url_for('index')) 
         
     # df = pd.read_csv('event.csv')
     # df.append({'Event Name': name, 'Date': date },ignore_index=True)
@@ -41,12 +47,7 @@ def getdetails():
     # df.to_csv('event.csv', header=True, index=False)
     # print(df)
 
-        # return render_template('new.html',df=df.to_csv('event.csv',header=True,index=False))  
-        
-
-    return render_template('index.html')
-    return name	
- 
+    # return render_template('new.html',df=df.to_csv('event.csv',header=True,index=False))  
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=PORT)
